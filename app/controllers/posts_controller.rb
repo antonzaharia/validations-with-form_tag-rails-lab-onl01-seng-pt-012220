92801aee6b0431ec9716ca4e86f952f560041d45
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-03 16:37:24 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,13 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @updated_post = Post.new(post_params)
+    if @updated_post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
